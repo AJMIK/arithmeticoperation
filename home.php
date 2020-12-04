3b91bf91e88e2c670d09e27642099cba904c99b5
AJMIK <ajmimkabeer24@gmail.com>
2020-12-04 14:05:44 +0530

    web site created

---
+++ home.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">   
+</head>
+<body class="back2">
+    <nav class="navbar navbar-expand-sm bg-danger navbar-dark">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="addition.php">Add</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="subtraction.php">Sub</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="multiplication.php">Mul</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="division.php">Div</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="container">
+          <div class="row">
+              <div class="col col-12 col-sm-2"></div>
+              <div class="col col-12 col-sm-8">
+                  <center><p class="text">MATH GAMES</p></center>
+                  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="image/download.jpg" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="image/images.jpg" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="image/images.jpg" class="d-block w-100" alt="...">
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+              </div>
+              
+              
+            
+             
+                <div class="col col-12 col-sm-4"></div>
+                
+                         <div class="col col-12 col-sm-3">
+                            <center><p class="text">ABOUT</p></center>
+                            <div class="card" style="width: 18rem;">
+                                <img src="image/add.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">ADDITION</h5>
+                                  <p class="card-text">Addition is a mathematical operation. It is a process or action that you do with numbers.
+                                    The symbol for this process is + (plus). In this process we combine 2 numbers to make a bigger number.
+                                    Example: 4 + 5 means to combine the numbers 4 and 5. The result will be 9.
+                                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                              
+                                    <div class="card" style="width: 18rem;">
+                                        <img src="image/sub.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                          <h5 class="card-title">SUBTRACTION</h5>
+                                          <p class="card-text">Subtraction is an arithmetic operation that represents the operation of removing objects from a collection. The result of a subtraction is called a difference. Subtraction is signified by the minus sign, −. For example, in the adjacent picture, there are 5 − 2 apples—meaning 5 apples with 2 taken away, resulting in a total of 3 apples. Therefore, the difference of 5 and 2 is 3, that is, 5 − 2 = 3</p>
+                                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                      </div>
+                               
+                               
+                               <div class="card" style="width: 18rem;">
+                                <img src="image/mul.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">MULTIPLICATION</h5>
+                                  <p class="card-text">Multiplication is an arithmetic operation for finding the product of two numbers in mathematics. It is often represented by symbols such as {\displaystyle \times }{\displaystyle \times } and {\displaystyle \cdot }\cdot. Multiplication is the third operation in math, after addition which is the first, and subtraction which is the second. It can also be defined on non-number mathematical objects as well</p>
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                              </div>
+                                  <div class="card" style="width: 18rem;">
+                                    <img src="image/div.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">DIVISION</h5>
+                                      <p class="card-text">In mathematics, the word "division" means the operation which is the opposite of multiplication. Some symbols for division can be a slash ({\displaystyle /}{\displaystyle /}), a (fraction) line, or the division sign ({\displaystyle \div }{\displaystyle \div }),[1] as in:
+                                        {\displaystyle 6/3\,}{\displaystyle 6/3\,}    or    {\displaystyle {\frac {6}{3}}}{\displaystyle {\frac {6}{3}}}    or    {\displaystyle 6\div 3.}{\displaystyle 6\div 3.}</p>
+                                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                  </div>
+                              </div>
+                         </div>
+              
+              
+              <div class="col col-12 col-sm-2"></div>
+              
+
+          </div>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ subtraction.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>subtraction</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body class="back1">
+    <nav class="navbar navbar-expand-sm bg-danger navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="home.php">Home</a>
+              </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="addition.php">Add</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="subtraction.php">Sub</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="multiplication.php">Mul</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="division.php">Div</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="container">
+          <div class="row">
+              <div class="col col-12 col-sm-2"></div>
+              <div class="col col-12 col-sm-8">
+                  <center><p class="text2">SUBTRACTION</p></center>
+                  <table class="table">
+                      <tr>
+                          <td>FIRST NUMBER</td>
+                          <td><input type="text" class="form-control" id="num1"></td>
+                      </tr>
+                      <tr>
+                          <td>SECOND NUMBER</td>
+                          <td><input type="text" class="form-control" id="num2"></td>
+                      </tr>
+                      <tr>
+                          <td>RESULT</td>
+                          <td><input type="text" class="form-control" id=res></td>
+                      </tr>
+                      <tr>
+                          <td></td>
+                          <td><button onclick="subtraction()" class="btn btn-info">DIFFERENCE</button></td>
+                      </tr>
+                  </table>
+              </div>
+              <div class="col col-12 col-sm-2"></div>
+          </div>
+      </div>
+      <script>
+        function subtraction() 
+        {
+            
+          var getnum1=parseInt(document.getElementById("num1").value)
+            var getnum2=parseInt(document.getElementById("num2").value)
+         var result=getnum1-getnum2
+         document.getElementById("res").value=result
+
+        }
+    </script>
+</body>
+</html>
